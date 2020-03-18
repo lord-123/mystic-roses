9c5cef70a00cb64b7046719c970a772a4c8ea629
Matthew Abel <mattabel2002@gmail.com>
2020-03-18 09:41:04 +0000

    added speed customization

--- mystic-roses.py
+++ mystic-roses.py
@@ -5,6 +5,8 @@ from itertools import combinations
 numberOfPoints = int(input("number of points: "))
 size = int(input("size: "))
 
+speed = int(input("speed: "))
+
 angle = 2 * math.pi / numberOfPoints
 
 polarCoords = [(size, angle * i) for i in range(numberOfPoints)]
@@ -14,6 +16,7 @@ window = turtle.Screen()
 window.bgcolor("black")
 
 pen = turtle.Turtle(visible=False)
+pen.speed(speed)
 pen.color("white")
 pen.up()
 
