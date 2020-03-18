0c4eb79d51374d8af09785180c95666279df653f
Matthew Abel <mattabel2002@gmail.com>
2020-03-18 09:22:17 +0000

    initial commit

---
+++ mystic-roses.py
@@ -0,0 +1,26 @@
+import turtle
+import math
+from itertools import combinations
+
+numberOfPoints = int(input("number of points: "))
+size = int(input("size: "))
+
+angle = 2 * math.pi / numberOfPoints
+
+polarCoords = [(size, angle * i) for i in range(numberOfPoints)]
+cartesianCoords = [(i[0] * math.cos(i[1]), i[0] * math.sin(i[1])) for i in polarCoords]
+
+window = turtle.Screen()
+window.bgcolor("black")
+
+pen = turtle.Turtle()
+pen.color("white")
+pen.up()
+
+for i in combinations(cartesianCoords, 2):
+    pen.goto(*i[0])
+    pen.down()
+    pen.goto(*i[1])
+    pen.up()
+
+input()
